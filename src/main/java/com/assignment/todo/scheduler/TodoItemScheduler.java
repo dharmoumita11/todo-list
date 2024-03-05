7cb8e56a72e77f2266232591606566431c07e67f
dharmoumita11 <dharmoumita11@gmail.com>
2024-03-05 12:07:25 +0100

    add scheduling

--- src/main/java/com/assignment/todo/scheduler/TodoItemScheduler.java
+++ src/main/java/com/assignment/todo/scheduler/TodoItemScheduler.java
@@ -1,5 +1,24 @@
 package com.assignment.todo.scheduler;
 
+import com.assignment.todo.service.TodoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TodoItemScheduler {
+
+    private final TodoItemService todoItemService;
+
+    @Autowired
+    public TodoItemScheduler(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
+
+    //@Scheduled(cron = "0 * * * * *") // Runs every minute, adjust as necessary
+    @Scheduled(fixedDelay = 60000) // Every minute
+    public void updatePastDueItemsStatus() {
+        todoItemService.checkAndUpdateStatusForPastDueItems();
+    }
 
 }
---
+++ src/test/java/com/assignment/todo/scheduler/TodoItemSchedulerTest.java
@@ -0,0 +1,6 @@
+package com.assignment.todo.scheduler;
+
+import com.assignment.todo.BaseTestClass;
+
+public class TodoItemSchedulerTest extends BaseTestClass {
+}
