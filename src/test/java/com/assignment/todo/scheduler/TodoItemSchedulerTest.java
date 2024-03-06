e308d6c4c87f109e20ac98b9664110434e5578ce
dharmoumita11 <dharmoumita11@gmail.com>
2024-03-06 02:00:12 +0100

    added more uniti tests

--- src/test/java/com/assignment/todo/scheduler/TodoItemSchedulerTest.java
+++ src/test/java/com/assignment/todo/scheduler/TodoItemSchedulerTest.java
@@ -1,6 +1,27 @@
 package com.assignment.todo.scheduler;
 
 import com.assignment.todo.BaseTestClass;
+import com.assignment.todo.service.TodoItemService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class TodoItemSchedulerTest extends BaseTestClass {
+
+    @Mock
+    private TodoItemService todoItemService;
+
+    @InjectMocks
+    private TodoItemScheduler todoItemScheduler;
+
+    @Test
+    void whenUpdatePastDueItemsStatus_thenSuccess() {
+        todoItemScheduler.updatePastDueItemsStatus();
+
+        verify(todoItemService, times(1)).checkAndUpdateStatusForPastDueItems();
+    }
+
 }
